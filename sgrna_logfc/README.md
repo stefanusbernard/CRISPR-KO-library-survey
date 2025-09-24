945206471a45d8f84f5cc4adb8eb41b179d4a2b6
Stefanus Bernard <stefanus.bernard@gmail.com>
2025-09-24 18:00:11 +0100

    Changes to be committed: 	modified:   02_paralog_off-target-sgrna.rmd 	modified:   03_logfoldchange_off-target-sgrna.Rmd 	modified:   sgrna_logfc/README.md 	new file:   sgrna_logfc/__pycache__/function_to_normalize_sgRNA_LFC.cpython-313.pyc 	new file:   sgrna_logfc/__pycache__/function_to_normalize_sgrna_lfc.cpython-313.pyc 	new file:   sgrna_logfc/function_to_normalize_sgrna_lfc.py 	renamed:    sgrna_logfc/04_Normalize_sgRNA_LFC.ipynb -> sgrna_logfc/normalize_avana_lfc.ipynb 	new file:   sgrna_logfc/normalize_tkov3_lfc.ipynb

--- sgrna_logfc/README.md
+++ sgrna_logfc/README.md
@@ -1,4 +1,12 @@
 # sgRNA log2fc file sources
 
 1. Avana library (AvanalogfoldChange.csv, DepMap 25Q2) --> https://depmap.org/portal/data_page/?tab=allData
+
 2. TKOv3 library (hap1.foldchange) --> https://academic.oup.com/g3journal/article/7/8/2719/6031511
+
+3. TKOv3 library (RPE1 cells drugZ; matrix-reads-by-gRNA-RPE1-drugZ.txt) --> https://link.springer.com/article/10.1186/s13073-019-0665-3#availability-of-data-and-materials & https://figshare.com/articles/dataset/Readcounts/8799215?file=16170896 
+
+4. TKOv3 library (Olivieri et al, 2020) --> https://data.mendeley.com/datasets/gfcn2wmrpf/1
+    - Dataset_S2_readcounts.txt (we only take Dataset_S2 because this read count data for drugs screened with TKOv3)
+    - Screen_Library.csv (not all treatment/replicates were performed in TKOv3!)
+    - TKOv3 containing 71090 guides targeting 18056 genes (Olivieri et al, 2020)
---
+++ sgrna_logfc/function_to_normalize_sgrna_lfc.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# function to normalize sgRNA LFC by median of all sgRNA (ignoring the NA)
+def normalize_column(col):
+    median = np.nanmedian(col)
+    centered = col - median
+    mad = np.nanmedian(np.abs(centered))
+    if mad == 0 or np.isnan(mad):
+        return centered
+    return centered / mad
+
+# function to scaled LFC by the absolute average LFC value across cell lines for guides targeting essential genes (Achilles Common Essential Controls)
+def scale_essential(df):
+    common_essential = pd.read_csv('AchillesCommonEssentialControls_25Q2.csv')
+    common_essential[['gene', 'accession']] = common_essential['Gene'].str.split(' ', n = 1, expand = True)
+    common_essential = common_essential[['gene', 'accession']]
+
+    list_common_essential = list(common_essential['gene'])
+    list_common_essential
+
+    # subset guides targeting essential genes
+    ess_guides = df.loc[df.index.get_level_values('gene').isin(list_common_essential)]
+
+    # compute average LFC across cell lines for each essential guides
+    ess_avg = ess_guides.mean(axis = 1)
+
+    # take absolute value and average across guides
+    scale_factor = np.mean(np.abs(ess_avg))
+    print('Scale factor: ', scale_factor)
+
+    df_scaled = df / scale_factor
+    
+    return df_scaled, list_common_essential
+
+def sanity_check_scale_essential(df, df_scaled, list_common_essential):
+    # Sanity check for the scaling LFC method
+    ess_before = df.loc[df.index.get_level_values('gene').isin(list_common_essential)].stack().values
+    ess_after = df_scaled.loc[df_scaled.index.get_level_values('gene').isin(list_common_essential)].stack().values
+
+    plt.boxplot([ess_before, ess_after], labels = ['Before', 'After'])
+    plt.axhline(y=0, color='r', linestyle='--')
+    plt.ylabel('logFC guides targeting common essential genes')
+    plt.show()
+    
+# function to correct LFC for gene copy number alterations 
+
+# function to average the row of the spacer
+def mean_row(df):
+    df['mean'] = df.mean(axis = 1)
+    df = df['mean']
+    return df
