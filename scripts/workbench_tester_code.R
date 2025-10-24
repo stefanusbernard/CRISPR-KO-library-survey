e5ebee4a3a644e52e99b18349c1a1045de05e77b
Stefanus Bernard <stefanus.bernard@gmail.com>
2025-10-24 19:44:31 +0100

    commit script

--- scripts/workbench_tester_code.R
+++ scripts/workbench_tester_code.R
@@ -9,31 +9,44 @@ source("./00_CRISPR-KO-library-survey-functions.R")
 source('~/CRISPR-KO-GuideRefine/GuideRefine_functions.R')
 
 
-terms <- c("CONTROL", "Control", "control", "INTRON", "Intron", "intron", "LacZ", "luciferase")
-# lacZ and luciferase are the control in Toronto V3 library
-
-terms_for_filtering <- paste(terms, collapse = "|")
-
-disposed_avana <- import_list_off_target_guides("~/CRISPR-KO-GuideRefine/output_cleaning/T2T-CHM13/avana_library_disposed_sgRNAs.tsv")
-
-annotation <- "~/CRISPR-KO-GuideRefine/annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds"
-genome_type_version <- "T2T-CHM13"
-bsgenome <- BSgenome.Hsapiens.NCBI.T2TCHM13v2.0
-
-# T2T-CHM13
-avana_alignment_meyers <- import_sgrna_library_alignment("~/CRISPR-KO-GuideRefine/object_intermediate/T2T-CHM13_bsgenome/avana_library_aln.csv", "T2T-CHM13")
-avana_classification <- off_target_classification(avana_alignment_meyers, disposed_avana["pam_distal_double_mismatch_guides"])
+combined_data
 
 
-avana_single_mismatch <- avana_classification[[1]] %>% filter(alignment == "single mismatch") %>% pull(sgRNA)
-avana_single_mismatch_df <- avana_alignment_meyers %>%
-  filter(sgRNA %in% avana_single_mismatch & n_mismatches == 1) %>%
-  arrange(sgRNA)
+stat.test <- combined_data %>%
+  group_by(library) %>%
+  pairwise_wilcox_test(
+    mean ~ alignment,     # paired comparison
+    paired = TRUE,    # indicates repeated measures
+    p.adjust.method = "bonferroni"
+  )
+
+combined_data <- combined_data %>%
+  mutate(xpos = paste0(library, "_", alignment))
 
 
-# Basic Kruskal–Wallis test
-kruskal.test(avana_library_and_lfc_data$mean ~ avana_library_and_lfc_data$alignment_bin, data = avana_library_and_lfc_data)
+combined_data$xpos <- factor(combined_data$xpos, levels = unique(combined_data$xpos))
 
-# Post-hoc pairwise comparisons (non-parametric)
-pairwise.wilcox.test(avana_library_and_lfc_data$mean, avana_library_and_lfc_data$alignment_bin, p.adjust.method = "BH")
+my_comp <- list(
+  c("brunello_perfect","brunello_multi-target guides"),
+  c("toronto_v3_perfect","toronto_v3_multi-target guides"),
+  c("yusa_perfect","yusa_multi-target guides"),
+  c("avana_perfect","avana_multi-target guides"),
+  c("jacquere_perfect","jacquere_multi-target guides")
+)
+
+ggboxplot(
+  combined_data, 
+  x = "library", 
+  y = "mean",
+  color = "alignment",        # differentiate datasets by color
+  palette = "jco",
+  outlier.shape = NA) + 
+  stat_compare_means(
+    comparisons = my_comp,
+    method = "wilcox.test",
+    label = "p.signif",
+    paired = FALSE,
+    aes(group = alignment)
+  )
+
 
