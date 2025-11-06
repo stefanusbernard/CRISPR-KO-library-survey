9cd1cb22ab504e3bf59cb159767eb5b66dcdf0c3
Stefanus Bernard <stefanus.bernard@gmail.com>
2025-11-06 18:36:24 +0000

    add figures and script

--- scripts/workbench_tester_code.R
+++ scripts/workbench_tester_code.R
@@ -4,13 +4,10 @@ library(ggvenn)
 library(ggbreak)
 library(BSgenome)
 library(BSgenome.Hsapiens.NCBI.T2TCHM13v2.0)
+library(HGNChelper)
 
 source("./00_CRISPR-KO-library-survey-functions.R")
 source('~/CRISPR-KO-GuideRefine/GuideRefine_functions.R')
-
-
-
-
 
 
 
