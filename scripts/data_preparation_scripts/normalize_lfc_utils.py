f66c552a7985f165c4a7c2f88937ec1029ee9739
Stefanus Bernard <79132254+stefanusbernard@users.noreply.github.com>
2026-06-21 18:50:21 +0100

    clean up metadata for all normalization files

--- scripts/data_preparation_scripts/normalize_lfc_utils.py
+++ scripts/data_preparation_scripts/normalize_lfc_utils.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # function to normalize sgRNA LFC by median of all sgRNA (ignoring the NA)
 def normalize_column(col):
